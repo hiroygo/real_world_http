82a3882946bc0e1330a49e213a1e45501852fb05
hiroygo <pcxn1a@gmail.com>
2020-11-08 21:03:19 +0900

    update

--- 5.3_xmlhttprequest/server.go
+++ 5.3_xmlhttprequest/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"time"
 )
 
 func changeServerState(conn net.Conn, state http.ConnState) {
@@ -26,8 +27,19 @@ func changeServerState(conn net.Conn, state http.ConnState) {
 	log.Println(s, conn.RemoteAddr())
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	//	dump, err := httputil.DumpRequest(r, true)
+	//	if err != nil {
+	//		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+	//		return
+	//	}
+	//	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>%s</body></html>\n", time.Now())
+}
+
 func main() {
 	httpServer := &http.Server{Addr: ":8080", ConnState: changeServerState}
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/date.html", handler)
 	fmt.Println(httpServer.ListenAndServe())
 }

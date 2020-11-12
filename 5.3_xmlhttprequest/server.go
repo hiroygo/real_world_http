d5c5b8e5c3dd646fc265807a53c248703eb16031
hiroygo <pcxn1a@gmail.com>
2020-11-12 23:21:48 +0900

    update

--- 5.3_xmlhttprequest/server.go
+++ 5.3_xmlhttprequest/server.go
@@ -9,22 +9,7 @@ import (
 )
 
 func changeServerState(conn net.Conn, state http.ConnState) {
-	var s string
-	switch state {
-	case http.StateNew:
-		s = "StateNew"
-	case http.StateActive:
-		s = "StateActive"
-	case http.StateIdle:
-		s = "StateIdle"
-	case http.StateHijacked:
-		s = "StateHijacked"
-	case http.StateClosed:
-		s = "StateClosed"
-	default:
-		panic("Unknown State")
-	}
-	log.Println(s, conn.RemoteAddr())
+	log.Println(state, conn.RemoteAddr())
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
